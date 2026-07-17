219dc27e7aed4d64285f08c1038122e25e09f0a8
muhammad nouman <muhammadnouman2662@gmail.com>
2026-07-17 09:15:05 +0500

    New

---
+++ js/firebase-config.js
@@ -0,0 +1,10 @@
+// Paste the config object from Firebase Console > Project settings > Your apps > SDK setup and configuration
+// Full steps are in SETUP.md
+export const firebaseConfig = {
+  apiKey: "AIzaSyBP-UG4tlGbTJaP037JAbR3HFrCN7N-Hq4",
+  authDomain: "itmetatechportfolio.firebaseapp.com",
+  projectId: "itmetatechportfolio",
+  storageBucket: "itmetatechportfolio.firebasestorage.app",
+  messagingSenderId: "592012538110",
+  appId: "1:592012538110:web:5f2a9a7fe5eac4faf2f5af"
+};
